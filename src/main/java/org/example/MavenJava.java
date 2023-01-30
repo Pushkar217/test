634b79ad181edcce780376eab9b1843c280d3617
Pushkar Yarraguntla <pushkaryarraguntla@gmail.com>
2023-01-30 12:09:09 -0600

    add all the files

---
+++ src/main/java/org/example/MavenJava.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MavenJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This is a project with maven installations");
+    }
+}
